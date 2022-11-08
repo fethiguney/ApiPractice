b69b5cb282f9118ebcbbb1202bbe0c45d7238ea3
Fethi <fethiguneyy@gmail.com>
2022-11-08 23:29:59 +0300

    08/11

--- src/test/java/day01/GetRequest03.java
+++ src/test/java/day01/GetRequest03.java
@@ -1,5 +1,11 @@
 package day01;
 
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class GetRequest03 {
 
 /* Matchers ile dataları doğrulayınız
@@ -9,4 +15,25 @@ public class GetRequest03 {
             "last_name": "Morris",
             "avatar": "https://reqres.in/img/faces/5-image.jpg"
      */
+
+
+    @Test
+    public void test03() {
+
+        //set the url
+        String url="https://reqres.in/api/users";
+
+        //set the expected data
+
+        //send the request and get response
+        Response response=given().when().get(url);
+
+        //Do assertion
+        response.then().body("data[4].email", equalTo("charles.morris@reqres.in"),
+                            "data[4].first_name", equalTo("Charles"),
+                            "data[4].last_name", equalTo("Morris"),
+                            "data[4].avatar", equalTo("https://reqres.in/img/faces/5-image.jpg"));
+
+
+    }
 }
---
+++ src/test/java/pojos/Country.java
@@ -0,0 +1,50 @@
+package pojos;
+
+public class Country {
+
+    private Integer id;
+    private String name;
+    private String states;
+
+    public Country() {
+    }
+
+    public Country(Integer id, String name, String states) {
+        this.id = id;
+        this.name = name;
+        this.states = states;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStates() {
+        return states;
+    }
+
+    public void setStates(String states) {
+        this.states = states;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", states='" + states + '\'' +
+                '}';
+    }
+}
